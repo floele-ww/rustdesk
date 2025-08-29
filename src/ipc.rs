756d712bfb33bb3278f9acb31dbcd55735152948
Florian Schmitz <schmitz@winworker.de>
2025-08-29 07:31:14 +0200

    Disable config update in ipc.rs

--- src/ipc.rs
+++ src/ipc.rs
@@ -548,23 +548,6 @@ async fn handle(data: Data, stream: &mut Connection) {
                 allow_err!(stream.send(&Data::Config((name, value))).await);
             }
             Some(value) => {
-                if name == "id" {
-                    Config::set_key_confirmed(false);
-                    Config::set_id(&value);
-                } else if name == "temporary-password" {
-                    password::update_temporary_password();
-                } else if name == "permanent-password" {
-                    Config::set_permanent_password(&value);
-                } else if name == "salt" {
-                    Config::set_salt(&value);
-                } else if name == "voice-call-input" {
-                    crate::audio_service::set_voice_call_input_device(Some(value), true);
-                } else if name == "unlock-pin" {
-                    Config::set_unlock_pin(&value);
-                } else {
-                    return;
-                }
-                log::info!("{} updated", name);
             }
         },
         Data::Options(value) => match value {
